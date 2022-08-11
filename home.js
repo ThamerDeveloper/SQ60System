e70bb0ed1b624e7dfe7641748a08ee5b6d722ce3
ThamerDeveloper <thamerdeve507@gmail.com>
2022-08-12 00:01:18 +0300

    Initial commit

--- home.js
+++ home.js
@@ -19,23 +19,6 @@ function showSlides() {
 }
 
 
-
-function msaad614() {
-    var elem = document.getElementById("myMsaad");   
-    var width = 0;
-    var id = setInterval(frame, 25);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-      } else {
-        width++; 
-        elem.style.width = width + '%'; 
-        elem.innerHTML = width * 1  + '%';
-      }
-    }
-  }
-
-
   //Get the button
 var mybutton = document.getElementById("myBtn");
 
@@ -54,4 +37,161 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+
+
+function msaad614() {
+  var elem = document.getElementById("myMsaad");   
+  var width = 0;
+  var id = setInterval(frame, 25);
+  function frame() {
+    if (width >= 72) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function nouf606() {
+  var elem = document.getElementById("myNouf");   
+  var width = 0;
+  var id = setInterval(frame, 25);
+  function frame() {
+    if (width >= 99) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function abdulk608() {
+  var elem = document.getElementById("myAbdulk");   
+  var width = 0;
+  var id = setInterval(frame, 25);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function jackr602() {
+  var elem = document.getElementById("myJackr");   
+  var width = 0;
+  var id = setInterval(frame, 25);
+  function frame() {
+    if (width >= 55) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function jackm607() {
+  var elem = document.getElementById("myJackm");   
+  var width = 0;
+  var id = setInterval(frame, 25);
+  function frame() {
+    if (width >= 93) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function mohj603() {
+  var elem = document.getElementById("myMouj");   
+  var width = 0;
+  var id = setInterval(frame, 25);
+  function frame() {
+    if (width >= 99) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function leon612() {
+  var elem = document.getElementById("myLeon");   
+  var width = 0;
+  var id = setInterval(frame, 25);
+  function frame() {
+    if (width >= 50) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function since613() {
+  var elem = document.getElementById("mySince");   
+  var width = 0;
+  var id = setInterval(frame, 25);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function mubarak616() {
+  var elem = document.getElementById("myMubarak");   
+  var width = 0;
+  var id = setInterval(frame, 25);
+  function frame() {
+    if (width >= 0) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function hamid615() {
+  var elem = document.getElementById("myHamid");   
+  var width = 0;
+  var id = setInterval(frame, 25);
+  function frame() {
+    if (width >= 0) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
+function abdoy617() {
+  var elem = document.getElementById("myAbdoy");   
+  var width = 0;
+  var id = setInterval(frame, 25);
+  function frame() {
+    if (width >= 0) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
 }
